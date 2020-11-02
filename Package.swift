d7a6f6786fa2a21ff8e83a9e6263be72f11889d9
sam38124 <sam38124@gmail.com>
2020-11-02 12:15:07 +0800

    1.0.1

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JzOsSerialization",
-            dependencies: []),
+            dependencies: ["JzOsSerialization","JzOsSqlHelper"]),
         .testTarget(
             name: "JzOsSerializationTests",
             dependencies: ["JzOsSerialization","JzOsSqlHelper"]),
--- Tests/JzOsSerializationTests/JzOsSerializationTests.swift
+++ Tests/JzOsSerializationTests/JzOsSerializationTests.swift
@@ -2,23 +2,18 @@ import XCTest
 @testable import JzOsSerialization
 
 final class JzOsSerializationTests: XCTestCase {
-    // 透過遵從 Codable 的協議，該 struct 即可被 Encodable 或 Decodable
+    // Need extend Codable
    public struct Device: Codable {
         var deviceType: String
         var deviceSize: Float
         var deviceVersion: String
     }
-
-    let device: Device = Device(deviceType: "iPhone 6s", deviceSize: 4.7, deviceVersion: "11.0.0")
-    let encoder: JSONEncoder = JSONEncoder()
-    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-//        XCTAssertEqual(JzOsUtil().text, "Hello, World!")
-        device.storeObject(name: "test")
+        //Store the object and give name
+        Device(deviceType: "iPhone 6s", deviceSize: 4.7, deviceVersion: "11.0.0").storeObject(name: "test")
+        //Use name to get object
         let a:JzOsSerializationTests.Device? = "test".getObject()
+        //Will reback iPhone 6s / 4.7 / 11.0.0
         print( a!.deviceType)
         print( a!.deviceSize)
         print( a!.deviceVersion)
