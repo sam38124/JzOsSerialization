41af4f10ad64a8dec3ef4fcc598f9a26b0a6a408
sam38124 <sam38124@gmail.com>
2020-11-02 12:21:35 +0800

    1.0.2

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "JzOsSerialization",
-            dependencies: ["JzOsSerialization","JzOsSqlHelper"]),
+            dependencies: ["JzOsSqlHelper"]),
         .testTarget(
             name: "JzOsSerializationTests",
             dependencies: ["JzOsSerialization","JzOsSqlHelper"]),
